2602b06036432c6e65900d01be6ef7cef8c40fd2
Aman <AmanA@hexaware.com>
2019-01-19 15:30:29 +0530

    initial file upload

---
+++ database/database.ddl
@@ -0,0 +1,10 @@
+-- creating Database
+create database FTP112;
+
+-- using the Database
+use FTP112;
+
+-- Creating Menu table 
+CREATE TABLE MENU(FOOD_ID INT PRIMARY KEY)
+
+
---
+++ restservice/canteenmanagement/src/main/java/com/hexaware/FTP112/model/Menu.java
@@ -0,0 +1,77 @@
+package com.hexaware.FTP112.model;
+
+import com.hexaware.FTP112.persistence.DbConnection;
+import com.hexaware.FTP112.persistence.MenuDAO;
+
+import java.util.Objects;
+import java.util.List;
+
+/**
+ * Menu class used to display menu information.
+ * @author hexware
+ */
+public class Menu {
+/**
+ * foodId to store foodId.
+ */
+  private int foodId;
+  /**
+   * Default Constructor.
+   */
+  public Menu() {
+
+  }
+/**
+ * @param argFoodId to initialize food id.
+ * used to get details through constructor.
+ */
+  public Menu(final int argFoodId) {
+    this.foodId = argFoodId;
+  }
+  @Override
+    public final boolean equals(final Object obj) {
+    if (obj == null) {
+      return false;
+    }
+    if (getClass() != obj.getClass()) {
+      return false;
+    }
+    Menu menu = (Menu) obj;
+    if (Objects.equals(foodId, menu.foodId)) {
+      return true;
+    }
+    return false;
+  }
+  @Override
+    public final int hashCode() {
+    return Objects.hash(foodId);
+  }
+    /**
+     * @return this food ID.
+     */
+  public final int getFoodId() {
+    return foodId;
+  }
+    /**
+     * @param argFoodId gets the food id.
+     */
+  public final void setFoodId(final int argFoodId) {
+    this.foodId = argFoodId;
+  }
+  /**
+   * Call the data base connection.
+   * @return the connection object.
+   */
+  private static MenuDAO dao() {
+    DbConnection db = new DbConnection();
+    return db.getConnect().onDemand(MenuDAO.class);
+  }
+  /**
+   * Call the data base connection.
+   * @return the array of menu object.
+   */
+  public static Menu[] showMenu() {
+    List<Menu> menu = dao().show();
+    return menu.toArray(new Menu[menu.size()]);
+  }
+}
